a275072c3e8ecdb8e4fbb727ca2fdcea16a31bd0
Marcus Wiseman <md.wiseman@hotmail.com>
2022-02-07 21:03:12 +0100

    More new card

--- src/routes/card/index.svelte
+++ src/routes/card/index.svelte
@@ -1,8 +1,8 @@
 <script>
     const recipe = {
         title: '',
-        ingredients: [''],
-        steps: [''],
+        ingredients: ['', ''],
+        steps: ['', '', ''],
     };
 </script>
 
@@ -12,24 +12,37 @@
 
 <h1 class="text-3xl font-semibold mb-3">Add new card</h1>
 
-<div class=" border rounded-lg shadow-xl p-6">
-    <label for="title" class="mr-4">Recipe name</label>
-    <input
-        type="text"
-        bind:value={recipe.title}
-        class="bg-transparent ring-2 ring-white focus:ring-2 focus:ring-violet-700 border-violet-700 rounded-lg"
-    />
+<div class="border rounded-lg shadow-xl p-6">
+    <div class="grid grid-cols-3 gap-4">
+        <label for="title" class="text-right text-2xl font-semibold">Recipe name</label>
+        <input type="text" bind:value={recipe.title} class="bg-violet-400 rounded-lg" />
+    </div>
 
-    <h3 class="text-xl">Ingredient</h3>
-    <div class="">
+    <h3 class="grid grid-cols-3 mt-6 mb-1"><span class="text-2xl text-right mr-3 font-semibold">Ingredients</span></h3>
+    <div class="grid grid-cols-3 gap-4">
         {#each recipe.ingredients as ingredient, i}
-            <label for="ingredient-{i}" class="">Ingredient {i + 1}</label>
-            <input type="text" id="ingridient-{i}" bind:value={recipe.ingredients[i]} class="" />
+            <label for="ingredient-{i}" class="text-right">Ingredient {i + 1}</label>
+            <input
+                type="text"
+                id="ingridient-{i}"
+                bind:value={recipe.ingredients[i]}
+                class="bg-violet-400 rounded-lg"
+            />
+            <span />
         {/each}
     </div>
 
-    {#each recipe.steps as step, i}
-        <label for="step-{i}" class="">Step {i + 1}</label>
-        <input type="text" id="step-{i}" bind:value={recipe.steps[i]} class="" />
-    {/each}
+    <h3 class="grid grid-cols-3 mt-6 mb-1"><span class="text-2xl text-right mr-3 font-semibold">Instructions</span></h3>
+    <div class="grid grid-cols-3 gap-4">
+        {#each recipe.steps as step, i}
+            <label for="step-{i}" class="text-right">Step {i + 1}</label>
+            <textarea
+                id="step-{i}"
+                cols="30"
+                rows="3"
+                bind:value={recipe.steps[i]}
+                class="bg-violet-400 rounded-lg col-span-2"
+            />
+        {/each}
+    </div>
 </div>
